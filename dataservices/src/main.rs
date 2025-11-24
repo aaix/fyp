83e9a91cf428a47da14a3d97d239d026ca9962c4
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2025-11-24 02:20:41 +0000

    UserService proto sample impl

--- dataservices/src/main.rs
+++ dataservices/src/main.rs
@@ -1,5 +1,16 @@
+use tokio;
+use tonic::transport::Server;
 
 
-fn main() {
+use dataservices::user::account::ScyllaUserService;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let addr = "[::1]:3114".parse().unwrap();
+
+    Server::builder()
+        .add_service(ScyllaUserService::service())
+        .serve(addr)
+        .await.unwrap();
 }
--- dataservices/src/user/account.rs
+++ dataservices/src/user/account.rs
@@ -1,3 +1,77 @@
+
 /*
 TABLE user
 */
+use tonic::{Request, Response, Status};
+
+use user_service::user_service_server::{UserService, UserServiceServer};
+use user_service::{CreateUserRequest, ReadUserResponse};
+
+use user_service::{DeleteUserRequest, DeleteUserResponse, ReadUserRequest, UpdateUserRequest};
+
+
+pub mod user_service {
+    tonic::include_proto!("dataservices.userproto");
+}
+
+
+fn create_response() -> ReadUserResponse {
+    ReadUserResponse {
+        user_id:"1234".to_string(),
+        avatar_asset_id: "12345".to_string(),
+        created_at: 0,
+        public_key: "real key :smirk_cat:".to_string(),
+        username: "john".to_string(),
+    }
+}
+
+
+#[derive(Debug)]
+pub struct ScyllaUserService;
+
+impl ScyllaUserService {
+    pub fn service() -> UserServiceServer<ScyllaUserService> {
+        UserServiceServer::new(Self {})
+    }
+}
+
+#[tonic::async_trait]
+impl UserService for ScyllaUserService {
+    async fn create_user(
+        &self,
+        request: Request<CreateUserRequest>,
+    ) -> Result<Response<ReadUserResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        Ok(Response::new(create_response()))
+    }
+
+    async fn read_user(
+        &self,
+        request: Request<ReadUserRequest>,
+    ) -> Result<Response<ReadUserResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        Ok(Response::new(create_response()))
+    }
+
+    async fn update_user(
+        &self,
+        request: Request<UpdateUserRequest>,
+    ) -> Result<Response<ReadUserResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        
+        Ok(Response::new(create_response()))
+    }
+
+    async fn delete_user(
+        &self,
+        request: Request<DeleteUserRequest>,
+    ) -> Result<Response<DeleteUserResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let response = DeleteUserResponse {};
+        Ok(Response::new(response))
+    }
+}
