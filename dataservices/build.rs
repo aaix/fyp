9711976973f120ec01a11c0260863673ba56c0af
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2025-11-29 21:06:37 +0000

    better proto structure

--- dataservices/build.rs
+++ dataservices/build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = tonic_prost_build::Config::new();
     config.protoc_arg("--experimental_allow_proto3_optional");
-    tonic_prost_build::configure().compile_with_config(config, &["proto/scyllauser.proto"], &["proto"])?;
+    let protos = ["proto/plib.proto", "proto/user.proto"];
+    tonic_prost_build::configure().compile_with_config(config, &protos, &["proto"])?;
     Ok(())
 }
---
+++ dataservices/src/protos.rs
@@ -0,0 +1,8 @@
+pub mod plib {
+    tonic::include_proto!("dataservices.plib");
+}
+
+pub mod user_service {
+    tonic::include_proto!("dataservices.userproto");
+}
+
