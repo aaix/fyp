9d28239b1ff1454aab78dfe30d4a39d0049e6385
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2025-11-29 21:52:47 +0000

    PUuid from impls

--- dataservices/src/helpers.rs
+++ dataservices/src/helpers.rs
@@ -10,3 +10,30 @@ impl From<uuid::Uuid> for PUuid {
     }
 }
 
+impl From<PUuid> for uuid::Uuid {
+    fn from(puuid: PUuid) -> Self {
+        uuid::Uuid::from_u64_pair(puuid.id_high, puuid.id_low)
+    }
+}
+
+impl From<PUuid> for scylla::value::CqlTimeuuid {
+    fn from(puuid: PUuid) -> Self {
+        let uuid: uuid::Uuid = puuid.into();
+        scylla::value::CqlTimeuuid::from(uuid)
+    }
+}
+
+impl From<scylla::value::CqlTimeuuid> for PUuid {
+    fn from(cql_timeuuid: scylla::value::CqlTimeuuid) -> Self {
+        let uuid: uuid::Uuid = cql_timeuuid.into();
+        uuid.into()
+    }
+}
+
+pub fn gen_timeuuid() -> scylla::value::CqlTimeuuid {
+    scylla::value::CqlTimeuuid::from(uuid::Uuid::new_v4())
+}
+
+pub fn gen_uuid() -> uuid::Uuid {
+    uuid::Uuid::new_v4()
+}
