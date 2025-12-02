82e72708120d137e87510143ce244efdb1d933c9
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2025-12-02 05:23:50 +0000

    move impls to corresponding mods

--- dataservices/src/helpers.rs
+++ dataservices/src/helpers.rs
@@ -2,39 +2,9 @@ use std::sync::OnceLock;
 use scylla::value::CqlTimeuuid;
 use mac_address::get_mac_address;
 
-use crate::protos::plib::PUuid;
 
 static NODE_ID: OnceLock<[u8; 6]> = OnceLock::new();
 
-impl From<uuid::Uuid> for PUuid {
-    fn from(uuid: uuid::Uuid) -> Self {
-        let (id_high, id_low) = uuid.as_u64_pair();
-        PUuid {
-            id_high,
-            id_low
-        }
-    }
-}
-
-impl From<PUuid> for uuid::Uuid {
-    fn from(puuid: PUuid) -> Self {
-        uuid::Uuid::from_u64_pair(puuid.id_high, puuid.id_low)
-    }
-}
-
-impl From<PUuid> for CqlTimeuuid {
-    fn from(puuid: PUuid) -> Self {
-        let uuid: uuid::Uuid = puuid.into();
-        CqlTimeuuid::from(uuid)
-    }
-}
-
-impl From<CqlTimeuuid> for PUuid {
-    fn from(cql_timeuuid: CqlTimeuuid) -> Self {
-        let uuid: uuid::Uuid = cql_timeuuid.into();
-        uuid.into()
-    }
-}
 
 pub fn gen_timeuuid() -> CqlTimeuuid {
     let node_id= *NODE_ID.get_or_init(|| {
--- dataservices/src/protos.rs
+++ dataservices/src/protos.rs
@@ -1,5 +1,37 @@
 pub mod plib {
     tonic::include_proto!("dataservices.plib");
+
+    // impls
+    use scylla::value::CqlTimeuuid;
+    impl From<uuid::Uuid> for PUuid {
+        fn from(uuid: uuid::Uuid) -> Self {
+            let (id_high, id_low) = uuid.as_u64_pair();
+            PUuid {
+                id_high,
+                id_low
+            }
+        }
+    }
+
+    impl From<PUuid> for uuid::Uuid {
+        fn from(puuid: PUuid) -> Self {
+            uuid::Uuid::from_u64_pair(puuid.id_high, puuid.id_low)
+        }
+    }
+
+    impl From<PUuid> for CqlTimeuuid {
+        fn from(puuid: PUuid) -> Self {
+            let uuid: uuid::Uuid = puuid.into();
+            CqlTimeuuid::from(uuid)
+        }
+    }
+
+    impl From<CqlTimeuuid> for PUuid {
+        fn from(cql_timeuuid: CqlTimeuuid) -> Self {
+            let uuid: uuid::Uuid = cql_timeuuid.into();
+            uuid.into()
+        }
+    }
 }
 
 pub mod user_service {
