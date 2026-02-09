8780f2c4a5bc70e0b2b21cf055a1f17cab418d28
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-02-09 15:08:47 +0000

    add prefix conf variables

--- api/crypto/session.py
+++ api/crypto/session.py
@@ -7,17 +7,17 @@ from cryptography.hazmat.primitives import hashes
 
 
 
-SIGN_KEY = jwk.generate_key("oct", 256)
-ENCRYPT_KEY = jwk.generate_key("oct", 256)
+CONF_SIGN_KEY = jwk.generate_key("oct", 256)
+CONF_ENCRYPT_KEY = jwk.generate_key("oct", 256)
 
 def encode_jose_session(data: str) -> str:
-    token = jws.serialize_compact({"alg": "HS256"}, data, private_key=SIGN_KEY, algorithms=["HS256"])
-    return jwe.encrypt_compact({"alg": "A256KW", "enc": "A256GCM"}, token, public_key=ENCRYPT_KEY, algorithms=["A256GCM", "A256KW"])
+    token = jws.serialize_compact({"alg": "HS256"}, data, private_key=CONF_SIGN_KEY, algorithms=["HS256"])
+    return jwe.encrypt_compact({"alg": "A256KW", "enc": "A256GCM"}, token, public_key=CONF_ENCRYPT_KEY, algorithms=["A256GCM", "A256KW"])
 
 def decode_jose_session(data: str) -> bytes:
-    token = jwe.decrypt_compact(data, private_key=ENCRYPT_KEY, algorithms=["A256GCM", "A256KW"])
+    token = jwe.decrypt_compact(data, private_key=CONF_ENCRYPT_KEY, algorithms=["A256GCM", "A256KW"])
     assert token.plaintext is not None
-    return jws.deserialize_compact(token.plaintext, public_key=SIGN_KEY, algorithms=["HS256"]).payload
+    return jws.deserialize_compact(token.plaintext, public_key=CONF_SIGN_KEY, algorithms=["HS256"]).payload
 
 def encrypt_session_with_key(session: str, raw_key: bytes) -> bytes:
     key: rsa.RSAPublicKey = serialization.load_pem_public_key(raw_key) # pyright: ignore[reportAssignmentType]
