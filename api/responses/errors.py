cc7cce762d9935f2937b44a2e934cea0c232ff2b
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-02-09 15:46:25 +0000

    order errors

--- api/responses/errors.py
+++ api/responses/errors.py
@@ -1,9 +1,10 @@
 from api.responses import ErrorResponse
 from api.responses.status_codes import *
 
-class InternalServerError(ErrorResponse):
-    API_CODE = ERROR_UNKNOWN
-    HTTP_CODE = 500
+
+class BadRequest(ErrorResponse):
+    API_CODE = ERROR_BAD_REQUEST
+    HTTP_CODE = 400
 
 class Unauthorized(ErrorResponse):
     API_CODE = ERROR_UNAUTHORIZED
@@ -13,14 +14,14 @@ class Forbidden(ErrorResponse):
     API_CODE = ERROR_FORBIDDEN
     HTTP_CODE = 403
 
-class BadRequest(ErrorResponse):
-    API_CODE = ERROR_BAD_REQUEST
-    HTTP_CODE = 400
+class NotFound(ErrorResponse):
+    API_CODE = ERROR_NOT_FOUND
+    HTTP_CODE = 404
 
 class UnsupportedMediaType(ErrorResponse):
     API_CODE = ERROR_BAD_REQUEST
     HTTP_CODE = 415
 
-class NotFound(ErrorResponse):
-    API_CODE = ERROR_NOT_FOUND
-    HTTP_CODE = 404
+class InternalServerError(ErrorResponse):
+    API_CODE = ERROR_UNKNOWN
+    HTTP_CODE = 500
