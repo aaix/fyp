a651eaac294a1e857e2aa25bf959a66f86918e0f
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2026-02-25 07:48:11 +0000

    redirect gateway too

--- ui/app/functions/api/[[path]].js
+++ ui/app/functions/api/[[path]].js
@@ -1,10 +1,17 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   
-  const targetHost = "api.az7.chat"; 
-  url.hostname = targetHost;
+  if (url.pathname.startsWith("/api")) {
+    url.hostname = "api.az7.chat";
+    
+    url.pathname = url.pathname.replace(/^\/api/, "");
+
+  } else if (url.pathname.startsWith("/gateway")) {
+    url.hostname = "gateway.az7.chat";
   
-  url.pathname = url.pathname.replace(/^\/api/, "");
+    url.pathname = url.pathname.replace(/^\/gateway/, "");
+
+  }
 
   const newRequest = new Request(url.toString(), context.request);
   
