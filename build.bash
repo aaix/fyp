cedb40db613358fc6d3e225aad0d7fda08da9706
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2026-03-04 07:19:30 +0000

    dataservices add dockerbuild script

--- build.bash
+++ build.bash
@@ -3,4 +3,4 @@
 docker build ./database -t az-scylla:latest
 . ./api/dockerbuild.sh
 . ./gateway/dockerbuild.sh
-docker build ./dataservices -t az-dataservices:latest
+. ./dataservices/dockerbuild.sh
