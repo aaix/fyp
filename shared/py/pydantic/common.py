d9a402524469774077bc4e988eeb8ceddb3ef118
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-03-05 20:34:26 +0000

    name input validation

--- shared/py/pydantic/common.py
+++ shared/py/pydantic/common.py
@@ -1,9 +1,18 @@
 from typing import Annotated
 
-from pydantic import Field
+from fastapi.exceptions import RequestValidationError
+from pydantic import Field, AfterValidator
 
 from shared.py.constraints import DEVICE_NAME_MAX_LENGTH, DEVICE_NAME_MIN_LENGTH, USERNAME_MAX_LENGTH, USERNAME_MIN_LENGTH
 
 
-type Username = Annotated[str, Field(max_length=USERNAME_MAX_LENGTH, min_length=USERNAME_MIN_LENGTH)]
-type DeviceName = Annotated[str, Field(max_length=DEVICE_NAME_MAX_LENGTH, min_length=DEVICE_NAME_MIN_LENGTH)]
+def str_is_printable_no_whitespace(string: str) -> str:
+    if not string.isprintable():
+        raise RequestValidationError("Unprintable character in input")
+    if ' ' in string:
+        raise RequestValidationError("Input must not contain whitespace")
+    return string
+
+
+type Username = Annotated[str,AfterValidator(str_is_printable_no_whitespace), Field(max_length=USERNAME_MAX_LENGTH, min_length=USERNAME_MIN_LENGTH)]
+type DeviceName = Annotated[str, AfterValidator(str_is_printable_no_whitespace), Field(max_length=DEVICE_NAME_MAX_LENGTH, min_length=DEVICE_NAME_MIN_LENGTH)]
