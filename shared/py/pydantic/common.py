3aa83a7920d40967c0d50780cbd94be2acfa79e5
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2026-03-08 05:27:56 +0000

    device names are allowed whitespace, fix 500 error from incorrect exception

--- shared/py/pydantic/common.py
+++ shared/py/pydantic/common.py
@@ -1,17 +1,17 @@
 from typing import Annotated
 
-from pydantic import Field, AfterValidator, ValidationError
+from pydantic import Field, AfterValidator
 
 from shared.py.constraints import DEVICE_NAME_MAX_LENGTH, DEVICE_NAME_MIN_LENGTH, USERNAME_MAX_LENGTH, USERNAME_MIN_LENGTH
 
 
 def str_is_printable_no_whitespace(string: str) -> str:
     if not string.isprintable():
-        raise ValidationError("Unprintable character in input")
+        raise ValueError("Unprintable character in input")
     if ' ' in string:
-        raise ValidationError("Input must not contain whitespace")
+        raise ValueError("Input must not contain whitespace")
     return string
 
 
-type Username = Annotated[str,AfterValidator(str_is_printable_no_whitespace), Field(max_length=USERNAME_MAX_LENGTH, min_length=USERNAME_MIN_LENGTH)]
-type DeviceName = Annotated[str, AfterValidator(str_is_printable_no_whitespace), Field(max_length=DEVICE_NAME_MAX_LENGTH, min_length=DEVICE_NAME_MIN_LENGTH)]
+type Username = Annotated[str, AfterValidator(str_is_printable_no_whitespace), Field(max_length=USERNAME_MAX_LENGTH, min_length=USERNAME_MIN_LENGTH)]
+type DeviceName = Annotated[str, Field(max_length=DEVICE_NAME_MAX_LENGTH, min_length=DEVICE_NAME_MIN_LENGTH)]
