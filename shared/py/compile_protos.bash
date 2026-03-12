3f696159f418eca02005b89f160f176160501ab9
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-03-12 16:42:12 +0000

    fix bug in compile protos import rewrite

--- shared/py/compile_protos.bash
+++ shared/py/compile_protos.bash
@@ -6,4 +6,4 @@ python -m grpc_tools.protoc \
   --grpc_python_out=./py/grpcgen \
   --pyi_out=./py/grpcgen \
   ../dataservices/proto/*.proto
-sed -i 's/import \([^ ]*\)_pb2/from . import \1_pb2/' ./py/grpcgen/*_pb2*.py
+sed -i 's/^import \([^ ]*\)_pb2/from . import \1_pb2/' ./py/grpcgen/*_pb2*.py
