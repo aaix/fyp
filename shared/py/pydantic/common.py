d585e282146355fb936ee5abcc525c76c676b6c3
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-03-16 11:44:34 +0000

    base64 constants

--- shared/py/pydantic/common.py
+++ shared/py/pydantic/common.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from pydantic import Field, AfterValidator
 
 from shared.py.constraints import *
+from shared.py.pydantic.base64 import Base64Output
 
 
 def str_is_printable_no_whitespace(string: str) -> str:
@@ -15,4 +16,4 @@ def str_is_printable_no_whitespace(string: str) -> str:
 
 type Username = Annotated[str, AfterValidator(str_is_printable_no_whitespace), Field(max_length=USERNAME_MAX_LENGTH, min_length=USERNAME_MIN_LENGTH)]
 type DeviceName = Annotated[str, Field(max_length=DEVICE_NAME_MAX_LENGTH, min_length=DEVICE_NAME_MIN_LENGTH)]
-type ChannelName = Annotated[bytes, Field(max_length=CHANNEL_NAME_MAX_LENGTH, min_length=CHANNEL_NAME_MIN_LENGTH)]
+type ChannelName = Annotated[Base64Output, Field(max_length=CHANNEL_NAME_MAX_LENGTH, min_length=CHANNEL_NAME_MIN_LENGTH)]
