ae9966b87a60e31c933581d1a6fef334a1437348
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-03-16 01:27:52 +0000

    fix github actions

--- shared/dockerbuild.sh
+++ shared/dockerbuild.sh
@@ -2,3 +2,4 @@ TAG=${1:-latest}
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}" && bash "${SCRIPT_DIR}/py/compile_protos.bash"
 docker build -t az-shared-base:${TAG} "${SCRIPT_DIR}"
+docker tag az-shared-base:${TAG} az-shared-base:latest # tag so that github actions can build it
