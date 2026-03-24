ee33fadb673fe34c861f8b00114ffe8355f23a19
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-03-24 03:10:44 +0000

    add forgotten constant

--- shared/py/constraints.py
+++ shared/py/constraints.py
@@ -20,6 +20,8 @@ CHANNEL_NAME_MIN_LENGTH: Final[int] = 34 # 48b64 34 ciphertext
 
 CHANNEL_MAX_NUM_MEMBERS: Final[int] = 15
 
+MESSAGE_CONTENT_MAX_LENGTH: Final[int] = 1000
+
 USER_MAX_NUM_DEVICES: Final[int] = 15
 
 MAX_MESSAGES_QUERYABLE: Final[int] = 50
