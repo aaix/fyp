627ed3cd09499f176b85d2a3a0e96d16e58ac605
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-03-25 02:47:28 +0000

    case insensitive usernames

--- shared/py/pydantic/common.py
+++ shared/py/pydantic/common.py
@@ -6,15 +6,15 @@ from shared.py.constraints import *
 from shared.py.pydantic.base64 import Base64Input, Base64Output
 
 
-def str_is_printable_no_whitespace(string: str) -> str:
+def str_is_printable_no_whitespace_lower(string: str) -> str:
     if not string.isprintable():
         raise ValueError("Unprintable character in input")
     if ' ' in string:
         raise ValueError("Input must not contain whitespace")
-    return string
+    return string.lower()
 
 
-type Username = Annotated[str, AfterValidator(str_is_printable_no_whitespace), Field(max_length=USERNAME_MAX_LENGTH, min_length=USERNAME_MIN_LENGTH)]
+type Username = Annotated[str, AfterValidator(str_is_printable_no_whitespace_lower), Field(max_length=USERNAME_MAX_LENGTH, min_length=USERNAME_MIN_LENGTH)]
 type DeviceName = Annotated[str, Field(max_length=DEVICE_NAME_MAX_LENGTH, min_length=DEVICE_NAME_MIN_LENGTH)]
 
 type ChannelNameIn = Annotated[Base64Input, Field(max_length=CHANNEL_NAME_MAX_LENGTH, min_length=CHANNEL_NAME_MIN_LENGTH)]
