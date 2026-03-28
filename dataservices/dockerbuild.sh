05a437e220033b5fc89a920a2dfd3358cfaf2eea
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2026-03-28 07:49:19 +0000

    dataservices debug mode

--- dataservices/dockerbuild.sh
+++ dataservices/dockerbuild.sh
@@ -4,4 +4,11 @@ REPO_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
 # shellcheck source=docker-build.inc.sh
 source "${REPO_ROOT}/docker-build.inc.sh"
 python "${REPO_ROOT}/dataservices/model_generator.py"
-docker_build_ci_aware dataservices -t "az-dataservices:${TAG}" --build-context "shared=${REPO_ROOT}/shared/" --build-context "root=${REPO_ROOT}" "${REPO_ROOT}/dataservices"
+if [ "${GITHUB_ACTIONS:-}" != "true" ]; then
+  release=""
+  mode="debug"
+else
+  release="--release"
+  mode="release"
+fi
+docker_build_ci_aware dataservices -t "az-dataservices:${TAG}" --build-context "shared=${REPO_ROOT}/shared/" --build-context "root=${REPO_ROOT}" --build-arg mode="${mode}" --build-arg release="${release}" "${REPO_ROOT}/dataservices"
