0cbcb5b9daf231b23c8daf1e1942a25e1424a60a
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2026-04-08 23:35:17 +0100

    system message should update bucket

--- api/routes/channel/system.py
+++ api/routes/channel/system.py
@@ -1,16 +1,19 @@
 
 
 from api.tracing import tracer
+from shared.py.grpc.channel import edit_channel
 from shared.py.grpcgen.internalmessage_pb2 import EventMessageCreate
 from shared.py.intraservice import client as intraclient
 from shared.py.discovery import DiscoveryManager
 from shared.py.grpc.id import id_puuid, id_t
 from shared.py.grpc.lazy import DataservicesLazyGRPC
 from shared.py.grpc.message import MessageType, create_message
-from shared.py.grpcgen import channel_pb2, message_pb2_grpc
+from shared.py.grpcgen import channel_pb2, channel_pb2_grpc, message_pb2_grpc
 
 discovery = DiscoveryManager()
 grpcmessage = DataservicesLazyGRPC(message_pb2_grpc.MessageServiceStub)
+grpcchannel = DataservicesLazyGRPC(channel_pb2_grpc.ChannelServiceStub)
+
 
 @tracer.start_as_current_span("create_system_message")
 async def create_system_message(channel: channel_pb2.ChannelObjectResponse, author_id: id_t, message_type: MessageType, content: bytes | None = None, fan_out: bool = True):
@@ -35,4 +38,8 @@ async def create_system_message(channel: channel_pb2.ChannelObjectResponse, auth
         message_type=message_type.value,
         content=content,
     ))
+
+    if channel.latest_bucket != message.bucket:
+        await edit_channel(grpcchannel, channel.channel_id, latest_bucket=message.bucket)
+
     return message
