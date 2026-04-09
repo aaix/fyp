1aa7dcd50b6210f0f014f1dfe712c092bb655ab3
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2026-04-09 04:47:48 +0100

    post gc plan

--- garbagecollector/processors/post.py
+++ garbagecollector/processors/post.py
@@ -4,4 +4,9 @@ from shared.py.grpc.id import id_t
 
 @tracer.start_as_current_span("process_post")
 async def process_post(bucket: int, post_id: id_t):
-    ...
+    # if post is large
+    # buckets = USER_EPOCH..bucket_now
+    # else buckets = fetch post like buckets
+
+    # delete post likes bucket at a time (partition at a time)
+    pass
