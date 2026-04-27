475e26afcdee97d2036e7db2de785e318e2542c3
aidan (wsl) <55102797+aaix@users.noreply.github.com>
2026-04-27 02:15:27 +0100

    api should use -u to flush on print

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -4,14 +4,14 @@
 MODE=${DEPLOYMENT_MODE:-dev}
 
 if [ "$MODE" = "prod" ]; then
-    exec uvicorn api.main:app \
+    exec python3 -u -m uvicorn api.main:app \
         --host 0.0.0.0 \
         --port 80 \
         --proxy-headers \
         --forwarded-allow-ips='*' \
         --app-dir /az7
 else
-    exec uvicorn api.main:app \
+    exec python3 -u -m uvicorn api.main:app \
         --host 0.0.0.0 \
         --port 8000 \
         --app-dir /az7
