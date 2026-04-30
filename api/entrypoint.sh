397dbd346db0a5795094ce1b256be71b09ba6ed8
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-04-30 21:54:25 +0100

    workers in prod

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -10,6 +10,7 @@ if [ "$MODE" = "prod" ]; then
         --proxy-headers \
         --forwarded-allow-ips='*' \
         --app-dir /az7
+        --workers 8
 else
     exec python3 -u -m uvicorn api.main:app \
         --host 0.0.0.0 \
