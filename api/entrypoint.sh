8dd445e80bfc17b60e679f52d51afcd945bdf12b
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-04-30 21:54:43 +0100

    escape newline

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -9,12 +9,12 @@ if [ "$MODE" = "prod" ]; then
         --port 80 \
         --proxy-headers \
         --forwarded-allow-ips='*' \
-        --app-dir /az7
+        --app-dir /az7 \
         --workers 8
 else
     exec python3 -u -m uvicorn api.main:app \
         --host 0.0.0.0 \
         --port 8000 \
-        --app-dir /az7
+        --app-dir /az7 \
         --workers 8
 fi
