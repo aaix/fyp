0e6de7662d135edfb5132ef4b55dcf5cdefdecbb
aidan (laptop wsl) <55102797+aaix@users.noreply.github.com>
2026-05-02 18:13:28 +0100

    use awscrt for signed url creation

--- shared/py/asset/__init__.py
+++ shared/py/asset/__init__.py
@@ -2,8 +2,12 @@ from collections.abc import Iterable
 
 
 import asyncio
+import datetime
 import threading
+
 import aioboto3
+
+from awscrt import auth, http
 from pydantic import BaseModel
 
 
@@ -16,6 +20,12 @@ discovery = DiscoveryManager()
 ENDPOINT_URL, ACCESS_KEY_ID, ACCESS_KEY_SECRET = discovery.find_s3_creds()
 PUBLIC_BUCKET, PRIVATE_BUCKET = discovery.find_s3_buckets()
 
+ENDPOINT_HOST = ENDPOINT_URL.replace("https://", "")
+
+
+print(ENDPOINT_HOST, flush=True)
+print(ENDPOINT_URL, flush=True)
+
 __all__ = (
     "client",
     "PUBLIC_BUCKET",
@@ -26,6 +36,12 @@ __all__ = (
 session = aioboto3.Session(
     aws_access_key_id=ACCESS_KEY_ID,
     aws_secret_access_key=ACCESS_KEY_SECRET,
+)
+
+
+AWSCRT_CREDENTIALS_PROVIDER = auth.AwsCredentialsProvider.new_static(
+    access_key_id=ACCESS_KEY_ID,
+    secret_access_key=ACCESS_KEY_SECRET,
 )
 
 
@@ -108,14 +124,15 @@ async def generate_signed_get(
 
 
     with tracer.start_as_current_span("s3.generate_signed") as span:
-        presigned =  await s3.generate_presigned_url(
-            'get_object',
-            Params={
-                "Bucket": bucket,
-                "Key": path,
-            },
-            ExpiresIn=duration,
-        )
+        presigned = _generate_crt_presigned_url(bucket, path, duration)
+        # await s3.generate_presigned_url(
+        #     'get_object',
+        #     Params={
+        #         "Bucket": bucket,
+        #         "Key": path,
+        #     },
+        #     ExpiresIn=duration,
+        # )
         span.set_attribute("az.shared.s3.signed", str(presigned))
     
     return presigned
@@ -164,3 +181,38 @@ async def generate_signed_put(
 class PresignedParams(BaseModel):
     url: str
     fields: dict[str, str]
+
+
+
+def _generate_crt_presigned_url(
+    bucket: str, 
+    key: str,  
+    expiration_seconds,
+):
+
+    path = f"/{bucket}/{key}"
+    
+    request = http.HttpRequest(
+        method="GET",
+        path=path,
+        headers=http.HttpHeaders([("host", ENDPOINT_HOST)])
+    )
+
+    signing_config = auth.AwsSigningConfig(
+        algorithm=auth.AwsSigningAlgorithm.V4,
+        signature_type=auth.AwsSignatureType.HTTP_REQUEST_QUERY_PARAMS,
+        credentials_provider=AWSCRT_CREDENTIALS_PROVIDER,
+        region="auto",
+        service="s3",
+        date=datetime.datetime.now(datetime.timezone.utc),
+        expiration_in_seconds=expiration_seconds,
+        use_double_uri_encode=True,
+        should_normalize_uri_path=False,
+        signed_body_value="UNSIGNED-PAYLOAD",
+    )
+
+
+    auth.aws_sign_request(request, signing_config).result()
+
+    signed_url = f"{ENDPOINT_URL}{request.path}"
+    return signed_url
